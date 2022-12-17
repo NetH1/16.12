0d17858ab2b4ae0aafa314bf52a0cbca98ac027c
NetH1 <youtube_17@mail.ru>
2022-12-17 11:32:15 +0600

    commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import MainPage from "./components/MainPage.vue";
+import { ref } from '@vue/reactivity'
+const items = ref([
+            {id:1, image:'',dsctiption:'Что то написано', amount:4},
+            {id:2, image:'',dsctiption:'Что то написано', amount:2},
+            {id:3, image:'',dsctiption:'Что то написано', amount:5},
+            {id:4, image:'',dsctiption:'', amount:null},
+            {id:5, image:'',dsctiption:'', amount:null},
+            {id:6, image:'',dsctiption:'', amount:null},
+            {id:7, image:'',dsctiption:'', amount:null},
+            {id:8, image:'',dsctiption:'', amount:null},
+            {id:9, image:'',dsctiption:'', amount:null},
+            {id:10, image:'',dsctiption:'', amount:null},
+            {id:11, image:'',dsctiption:'', amount:null},
+            {id:12, image:'',dsctiption:'', amount:null},
+            {id:13, image:'',dsctiption:'', amount:null},
+            {id:14, image:'',dsctiption:'', amount:null},
+            {id:15, image:'',dsctiption:'', amount:null},
+            {id:16, image:'',dsctiption:'', amount:null},
+            {id:17, image:'',dsctiption:'', amount:null},
+            {id:18, image:'',dsctiption:'', amount:null},
+            {id:19, image:'',dsctiption:'', amount:null},
+            {id:20, image:'',dsctiption:'', amount:null},
+            {id:21, image:'',dsctiption:'', amount:null},
+            {id:22, image:'',dsctiption:'', amount:null},
+            {id:23, image:'',dsctiption:'', amount:null},
+            {id:24, image:'',dsctiption:'', amount:null},
+            {id:25, image:'',dsctiption:'', amount:null},
+        ])
+</script>
+
+<template>
+  <div class="bg-page">
+    <MainPage :items ='items'/>
+  </div>
+
+  <RouterView />
+</template>
+
---
+++ src/components/MainPage.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <div class="flex-content">
+      <div class="bg-border-content flex-1">
+        <img src="../assets/imgblur.png" alt="imgblur" />
+        <div>content</div>
+      </div>
+      <div class="bg-border-content flex-2">
+        <div class="w-full" v-for="item in items" :key="item.id">
+          <div class="bg-border-content flex-3">
+            <img src="../assets/green.png" alt="" />
+            <span class="span-absolute">{{ item.amount }}</span>
+          </div>
+          <div v-if="isShow == true">
+            <img src="../assets/green.png" alt="">
+            <p>{{item.desctiption}}</p>
+            <button>Удалить</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity';
+export default {
+    setup() {
+        
+        const isShow = ref(false)
+        return{
+            isShow
+        }
+    },
+  props: {
+    items: {
+      type: Array,
+    },
+  },
+  methods: {
+     fuyt(id) {
+        this.items.map(x => {
+          if(x.id == id) {
+            this.isShow = true
+          } else{
+            this.isShow = false
+          }
+          return x
+        })
+    }
+  }
+};
+</script>
+
